5bbc4bc6206e087303514ba55eda0c81d6deec4a
Andiry Xu <t-jianxu@microsoft.com>
2017-07-01 17:22:57 -0700

    Add nvm_cli.c

--- make.bat
+++ make.bat
@@ -7,5 +7,6 @@ gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_be_ioctl.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_be_lba.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_bounds.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_buf.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_cli.c
 
 
