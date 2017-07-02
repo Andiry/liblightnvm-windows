068ac806dd3165fc373a3c7dc18d8ee0f15eec08
Andiry Xu <t-jianxu@microsoft.com>
2017-07-01 17:50:42 -0700

    Add cmd/dev/geo.c

--- make.bat
+++ make.bat
@@ -8,5 +8,8 @@ gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_be_lba.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_bounds.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_buf.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_cli.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_cmd.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_dev.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_geo.c
 
 
