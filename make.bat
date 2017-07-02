7c20ade41ef3950aa6749d1c42cb20a3fb419e36
Andiry Xu <t-jianxu@microsoft.com>
2017-07-01 17:06:48 -0700

    Add nvm_be_lba.c

--- make.bat
+++ make.bat
@@ -3,6 +3,7 @@ cd build
 
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_addr.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_bbt.c
-gcc -Wall -fPIC -O3 -std=c1x -c ../src/nvm_be_ioctl.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_be_ioctl.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_be_lba.c
 
 
