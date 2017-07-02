7f50df4bbda8cd92a4451ad65215bc7d0cc70a0a
Andiry Xu <t-jianxu@microsoft.com>
2017-07-01 17:12:22 -0700

    Add nvm_buf.c nvm_bounds.c

--- make.bat
+++ make.bat
@@ -5,5 +5,7 @@ gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_addr.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_bbt.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_be_ioctl.c
 gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_be_lba.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_bounds.c
+gcc -Wall -fPIC -O3 -std=c11 -c ../src/nvm_buf.c
 
 
